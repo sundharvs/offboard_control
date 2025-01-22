777d3089e5234057400fdfd002d9c31eba335999
Sundhararajan Vinodh Sangeetha <sundharvs@gmail.com>
2025-01-22 01:00:20 -0500

    remove build artifacts

--- build/waypoint_nav/CMakeFiles/waypoint_nav_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/waypoint_nav_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/waypoint_nav_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
